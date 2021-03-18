a49233b0c3facd251ce637168f0aeccc120de42a
guisoacelga <guiso.acelga@gmail.com>
2021-03-18 10:02:17 +0100

    Refactor

--- src/ExPerfectNumbers.java
+++ src/ExPerfectNumbers.java
@@ -1,27 +1,33 @@
 public class ExPerfectNumbers {
 
         public static void main(String[] args) {
-            for (long i = 0; i <= 100000000L ; i++) {
-                if (isPerfect(i))
-                System.out.println(i + " is a perfect number.");
+            for (int i = 0; i <= 100000000L ; i++) {
+                if (isPerfectNumber(i))
+                System.out.println( i + " is a perfect number.");
             }
         }
 
-        public static boolean isPerfect (long number){
-            long counter = 1;
-            long sum = 0;
-            while (counter < number){
+        public static boolean isPerfectNumber(int number){
 
+            if ( calculateRealDivisors(number)== number){
+                return true;
+            }
+            return false;
+
+        }
+
+        public static int calculateRealDivisors (int number){
+            int counter;
+            int sum = 0;
+
+            for (counter =1; counter <= number / 2; counter++){
                 if (number % counter == 0){
                     sum = sum + counter;
                 }
-                counter ++;
             }
 
-            if (sum == number){
-                    return true;
-            }
-            return false;
+            return sum;
+
         }
 
 
---
+++ src/org/campus02/linkedlist/Node.java
@@ -0,0 +1,6 @@
+package org.campus02.linkedlist;
+
+public class Node {
+    int value;
+    Node next;
+}
